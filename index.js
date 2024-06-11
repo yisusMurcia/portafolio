45c0ab123d5bc328db3ac93eb5b7d71b04e41aa2
Yisus <yisusMurci@.com>
2024-06-11 10:44:16 -0500

    Desplegable futuros proyectos -Pendiente: Cerrar desplegable

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const botonAcercaDeMi = document.getElementById("acerca-de-mi");
+const futurosProyectos = [
+    "Triqui",
+    "Conecta 4"
+];
 
-botonAcercaDeMi.addEventListener("click", ()=>{
-    location.href= "acercaDeMi.html";
-})
+const abrirFuturosProyectos=()=>{//Crear ventana emergente
+    const mainEl = document.getElementsByTagName("main")[0];
+    
+    const sectionContainer = document.createElement("section");
+    sectionContainer.classList.add("emergente");
+
+    sectionContainer.innerHTML = `
+    <h2>Futuros Proyectos</h2>
+    <ul>`;
+    for(let i= 0; i < 3; i++){
+        sectionContainer.innerHTML += `<li>${futurosProyectos[i]}</li>`;
+    }
+    sectionContainer.innerHTML += `<ul>
+    <button id= "boton-cerrar">cerrar</button>`;
+    mainEl.appendChild(sectionContainer);
+}
+
+
+
+//Obtener botones
+const botonFuturosProyectos = document.getElementById("futuros-proyectos");
+const botonCerrar = document.getElementById("boton-cerrar");
+
+//Añadir eventos de los botones
+botonFuturosProyectos.addEventListener("click", ()=>{abrirFuturosProyectos()});
+if (!botonCerrar){
+    botonCerrar.addEventListener("click", ()=>{
+        
+    })
+}
