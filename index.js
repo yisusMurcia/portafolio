7e38366d46be1910fcf5e75320a7bf22a83d0af3
Yisus <yisusMurci@.com>
2024-07-16 11:58:58 -0500

    Mi primer proyecto -Se añadió mi primer proyecto -Se automatizó el proyecto de añadir el proyecto a la página -Se creo la media query para dispositivos más pequeños

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+const contenedorProyectos = document.getElementsByClassName("contenedor-proyectos")[0]
 const futurosProyectos = [
     "Triqui",
     "Conecta 4",
     "calculadora",
     "convertidor de medidas"
 ];
+
+const proyectos = [
+    {
+        titulo: "Triqui",
+        tecnologias: ["python"],
+        enlace: "https://github.com/yisusMurcia/triqui",
+        descripcion: "Un código para jugar el famoso juego de triqui o tres en raya, ya sea para jugar con otra persona o contra el algoritmo.",
+        img : 'triqui.png'
+    }
+]
+
+//Mostrar proyectos
+for (const proyecto of proyectos) {
+    const tarjetaProyecto = document.createElement("div");
+
+    tarjetaProyecto.classList.add("proyecto");
+
+    tarjetaProyecto.style.backgroundImage = `url(img/proyectos/${proyecto.img})`
+    
+    contenedorProyectos.appendChild(tarjetaProyecto)
+
+    //Llenar la tarjeta
+    tarjetaProyecto.innerHTML= `<h3>${proyecto.titulo}</h3>
+    <div class="contenedor-tecnologias"> ${proyecto.tecnologias.join("<hr>")}</div>
+    <p>${proyecto.descripcion}</p>
+    <a href="${proyecto.enlace}">Código</a>`
+
+}
 
 const abrirFuturosProyectos=()=>{//Crear ventana emergente
     const mainEl = document.getElementsByTagName("main")[0];
