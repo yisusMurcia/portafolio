e4cca384a63009d93c7a6e5032ce2e98ac635cf7
Yisus <yisusMurci@.com>
2024-10-20 19:27:29 -0500

    Mejora de estilo -Se rediseño la sección de proyectos -Se reaorganziaron las secciónes de proyectos -Se añadieron las imagenes de los proyectos -Se añadió el enlace a linkedin

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const contenedorProyectos = document.getElementsByClassName("contenedor-proyectos")[0]
-const futurosProyectos = [
-    "Triqui",
-    "Conecta 4",
-    "calculadora",
-    "convertidor de medidas"
-];
+const botonObtenerCorreo = document.getElementById("boton-copiar-correo");
+
 
 // Proyectos mostrados en la pantalla
 const proyectos = [
@@ -12,20 +8,20 @@ const proyectos = [
         titulo: "Triqui",
         enlace: "https://github.com/yisusMurcia/triqui",
         descripcion: "Un código para jugar el famoso juego de triqui o tres en raya, ya sea para jugar con otra persona o contra el algoritmo.", 
+        img: "triqui.png",
     },
     {
         titulo: "Menu",
         descripcion: "Un app para crear y editar un menu de comida",
         enlace: "https://github.com/yisusMurcia/menu",
+        img: "menu.png"
     },
     {
         titulo: "Playlist",
         descripcion: "Un programa para crear y editar una playlist de musica",
         enlace: "https://github.com/yisusMurcia/playlist",
+        img: "playlist.png"
     },
-    {
-        
-    }
 ]
 
 //Mostrar proyectos
@@ -33,13 +29,16 @@ for (const proyecto of proyectos) {
     const tarjetaProyecto = document.createElement("div");
 
     tarjetaProyecto.classList.add("proyecto");
-    tarjetaProyecto.style.backgroundImage = proyecto?.img? `url(img/proyectos/${proyecto.img})`: `url(img/proyectos/default.jpg)`;
     
     
     //Llenar la tarjeta
-    tarjetaProyecto.innerHTML= `<h3>${proyecto.titulo}</h3>
-    <hr>
-    <p>${proyecto.descripcion}</p>`;
+    tarjetaProyecto.innerHTML= `
+    <img src= "img/proyectos/${proyecto?.img? proyecto.img: 'default.jpeg'}">
+    <span>
+        <h3>${proyecto.titulo}</h3>
+        <hr>
+        <p>${proyecto.descripcion}</p>
+    </span>`;
     
     tarjetaProyecto.addEventListener("click", ()=>{
         window.open(proyecto.enlace, "_blank");
@@ -48,37 +47,5 @@ for (const proyecto of proyectos) {
     contenedorProyectos.appendChild(tarjetaProyecto)
 }
 
-const abrirFuturosProyectos=()=>{//Crear ventana emergente
-    const mainEl = document.getElementsByTagName("main")[0];
-    
-    const sectionContainer = document.createElement("section");
-    sectionContainer.classList.add("emergente");
-
-    sectionContainer.innerHTML = `<h2>Futuros Proyectos</h2>
-    <ul>`;
-    for(let i= 0; i < futurosProyectos.length; i++){
-        sectionContainer.innerHTML += `<li>${futurosProyectos[i]}</li>`;
-    }
-    sectionContainer.innerHTML += `<ul>
-    <br>`;
-
-    mainEl.appendChild(sectionContainer);
-    
-    document.addEventListener("click", (e)=>{
-        const el = e.target;
-        if (el != sectionContainer && el != botonFuturosProyectos){//Cerrar la ventana al clickear otro elemento
-            sectionContainer.remove();
-        }
-    })
-    
-}
-
-
-
-//Obtener botones
-const botonFuturosProyectos = document.getElementById("futuros-proyectos");
-const botonObtenerCorreo = document.getElementById("boton-copiar-correo");
-
 //Añadir eventos de los botones
-botonFuturosProyectos.addEventListener("click", ()=>{abrirFuturosProyectos()});
 botonObtenerCorreo.addEventListener("click", ()=>{navigator.clipboard.writeText("yisusmurci@gmail.com")})
